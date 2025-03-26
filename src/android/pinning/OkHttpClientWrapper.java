5c6303847ab08210955b662701aa43032bad4426
Paulo Cesar Camilo <122461054+pcamilojunior@users.noreply.github.com>
2025-03-26 17:46:30 +0100

    OkHttpClientWrapper - avoid recreating OkhttpBuild every time
    OkHttpClientWrapper - avoid recreating OkhttpBuild every time

--- src/android/pinning/OkHttpClientWrapper.java
+++ src/android/pinning/OkHttpClientWrapper.java
@@ -1,16 +1,24 @@
 package com.outsystems.plugins.sslpinning.pinning;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 
 public class OkHttpClientWrapper {
 
     private static OkHttpClientWrapper instance;
+    int timeoutSeconds = 30;
 
     private OkHttpClient client;
     private OkHttpClientWrapper() {
         if(client == null) {
             OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
-            clientBuilder.retryOnConnectionFailure(false);
+            clientBuilder.connectTimeout(timeoutSeconds, TimeUnit.SECONDS);
+            clientBuilder.readTimeout(timeoutSeconds, TimeUnit.SECONDS);
+            clientBuilder.retryOnConnectionFailure(true);
+            clientBuilder.protocols(Collections.singletonList(Protocol.HTTP_1_1));
             client = clientBuilder.build();
         }
     }
